475f6c21349d097e05d4dbbb3274dd195f39fd7b
Zach-Deladev <zach.delapenha.developer@gmail.com>
2022-07-17 04:54:30 +0000

    Added loader to my gallery.

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,44 +1,41 @@
 import React, { Component } from "react";
 import css from "./css/Content.module.css";
-import {savedPosts} from "../posts.json";
+import { savedPosts } from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
-    constructor(props) {
-        super(props)
-    }
-    
-    render() {
-        return (
-            <div className={css.Content}>
-                
-                <div className={css.TitleBar}>
-                    <h1>My Photos</h1>
-                </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true,
+      });
+    }, 2000);
+  }
 
-                <div className={css.SearchResults}>
+  render() {
+    return (
+      <div className={css.Content}>
+        <div className={css.TitleBar}>
+          <h1>My Photos</h1>
+        </div>
 
-                    {/* Part 1: Creating the map function */}
-
-                        {/* {
-                    savedPosts.map((post)=>{
-                            return <div className={css.SearchItem} key={post.title}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt="random"/>
-                                <p>{post.description}</p>
-                                </div>
-                        })
-                    } */}
-
-
-                    {/* Part 2: Creating a child component */}
-
-                    <PostItem savedPosts={savedPosts} />
-                </div>
-            </div>
-        )
-    }
+        <div className={css.SearchResults}>
+          {this.setState.isLoaded ? (
+            <PostItem savedPosts={savedPosts} />
+          ) : (
+            <Loader />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Content
+export default Content;
